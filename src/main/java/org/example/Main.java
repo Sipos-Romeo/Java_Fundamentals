2d832ecbf966a59715f841900db4b339037aa543
sipos <siposromeo06@gmail.com>
2024-02-12 15:51:45 +0200

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 27;
+        int number2 = 5;
+        int sum = number1 + number2;
+        int diff = number1 - number2;
+        int prod = number1 * number2;
+        int div = number1 % number2;
+        int last_decimal = number1 %10;
+
+
+        System.out.println("Sum is: \t" + sum);
+        System.out.println("Diff is: \t" + diff);
+        System.out.println("Prod is \t" + prod);
+        System.out.println("Div is: \t" + div);
+        System.out.println("Last decimal is: \t" + last_decimal );
+
+
+                ;
+    }
+}
